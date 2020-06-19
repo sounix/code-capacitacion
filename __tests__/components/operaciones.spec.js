e72b17212dc2c46d8791670c16f3ab29511ea12d
Aldrin Gonzalez Cancino <sounix@gmail.com>
2020-06-18 19:50:19 -0500

    Correccion de componente suma

--- __tests__/components/operaciones.spec.js
+++ __tests__/components/operaciones.spec.js
@@ -1,9 +1,17 @@
 const { suma } = require("../../src/components/operaciones");
 
-describe("Test unitarios del componente operaciones", () => {
-  describe("Test de funcion suma", () => {
-    it("suma 1 + 2 = 3", () => {
-      expect(suma(1, 2)).toEqual(3);
-    });
+describe("Componente suma", () => {
+  it("Deberia mostrar la suma de 2 numeros", () => {
+    const { a, b } = { a: 2, b: 5 };
+
+    expect(suma(a, b)).toEqual(7);
+  });
+
+  it("Solo deberia aceptar numeros", () => {
+    const { a, b } = { a: "a", b: 5 };
+
+    expect(suma(a, b)).toBe(null);
+    expect(suma(2, "aldrin")).toBe(null);
+    expect(suma("Hola", "mundo")).toBe(null);
   });
 });
--- src/components/operaciones.js
+++ src/components/operaciones.js
@@ -1,5 +1,7 @@
 module.exports = {
   suma: (a, b) => {
+    if (typeof a !== "number") return null;
+    if (typeof b !== "number") return null;
     return a + b;
   },
   resta: (a, b) => {
