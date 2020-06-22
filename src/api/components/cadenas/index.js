68e4cc8cdd470a0a705bb557c96fa9275d8e8a49
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-06-22 12:24:30 -0500

    refactorizacion de componente cadenas

--- src/api/components/cadenas/index.js
+++ src/api/components/cadenas/index.js
@@ -1,31 +1,29 @@
 module.exports = {
     invierteCadena: (cadena) => {
-        if (typeof cadena != "string") return null;
-        const length_cadena = cadena.trim().length;
-        if (length_cadena == 0) return cadena;
+        if (typeof cadena !== "string" || cadena.trim().length === 0) return null;
         return cadena.split("").reverse().join("");
     },
-    esPalindromo: (cadena) => {
-        if (typeof cadena != "string" || cadena.trim().length == 0) return null;
+    isPalindromo: (cadena) => {
+        if (typeof cadena !== "string" || cadena.trim().length === 0) return null;
         let cad = (cadena.toLowerCase().split("")).map((item) => {
+            if(item === "á") return "a";
             if(item === "é") return "e";
             if(item === "í") return "i";
             if(item === "ó") return "o";
             if(item === "ú") return "u";
             return item;
-        }).join("");
-        const invertida = cad.split("").reverse().join("");
-        if (cad.replace(/\s/g,"") === invertida.replace(/\s/g,"")) return true;
+        }).join("").replace(/\s/g,"");
+        if (cad === cad.split("").reverse().join("")) return true;
         return false;
     },
     cadenaToSequelize: (cadena) => {        
-        if (typeof cadena != "string" || cadena.trim().length == 0) return null;
+        if (typeof cadena !== "string" || cadena.trim().length === 0) return null;
         const parametro = cadena.split(":");
-        if (parametro.length != 4) return null;
-        if (parametro[0] === "" || parametro[1] === "" || parametro[2] === "" || parametro[3] === "") return null;
-        if (parametro[1].split("/").length !== 3 || /@+/.test(parametro[2]) == false) return null;
+        if (parametro.length !== 4) return null;
+        if (parametro[0].trim() === "" || parametro[1].trim() === "" || parametro[2].trim() === "" || parametro[3].trim() === "") return null;
+        if (parametro[1].split("/").length !== 3 || /@+/.test(parametro[2]) === false) return null;
         const port_and_dbname = parametro[3].split("/");
-        if (port_and_dbname.length != 2 || port_and_dbname[0].trim() == "" || port_and_dbname[1].trim() == "") return null;
-        return {database: port_and_dbname[1], username: (parametro[1].split("/"))[2], password: parametro[2].replace(/(.*)@(.*)/,'$1'), host: (parametro[2].replace(/(.*)@(.*)/,'$2')), dialect: parametro[0]};
+        if (port_and_dbname.length !== 2 || port_and_dbname[0].trim() === "" || port_and_dbname[1].trim() === "") return null;
+        return {database: port_and_dbname[1], username: (parametro[1].split("/"))[2], password: parametro[2].replace(/(.*)@(.*)/,'$1'), host: (parametro[2].replace(/(.*)@(.*)/,'$2')), dialect: parametro[0], port: port_and_dbname[0]};
     },
 }
--- test/api/components/cadenas.spec.js
+++ test/api/components/cadenas.spec.js
@@ -1,5 +1,4 @@
-const { invierteCadena, esPalindromo, cadenaToSequelize } = require("../../../src/api/components/cadenas");
-const cadena = {cad1: "hola"}
+const { invierteCadena, isPalindromo, cadenaToSequelize } = require("../../../src/api/components/cadenas");
 describe("componente - cadenas", () =>{
     describe("invierteCadena", () => {
         it("deberia de existir la funcion", () =>{
@@ -13,9 +12,10 @@ describe("componente - cadenas", () =>{
             expect(invierteCadena(true)).toBe(null);
             expect(invierteCadena({a: ""})).toBe(null);
             expect(invierteCadena(["a"])).toBe(null);
+            expect(invierteCadena("      ")).toBe(null);
         });
         it("deberia de retornar la cadena invertida", () => {
-            expect(invierteCadena("      ")).toBe("      ");
+            const cadena = {cad1: "hola"}
             expect(invierteCadena("hola")).toBe("aloh");
             expect(invierteCadena("hola como estas")).toBe("satse omoc aloh");
             expect(invierteCadena(" hola")).toBe("aloh ");
@@ -26,30 +26,30 @@ describe("componente - cadenas", () =>{
         });
     });
 
-    describe("esPalindromo", () => {
+    describe("isPalindromo", () => {
         it("deberia de existir la funcion", () => {
-            expect(typeof esPalindromo).toBe("function");
+            expect(typeof isPalindromo).toBe("function");
         });
         it('deberia de contener una cadena y no tiene que estar vacia', () => {
-            expect(esPalindromo(null)).toBe(null);
-            expect(esPalindromo(1)).toBe(null);
-            expect(esPalindromo(undefined)).toBe(null);
-            expect(esPalindromo(2.3)).toBe(null);
-            expect(esPalindromo(true)).toBe(null);
-            expect(esPalindromo({a: ""})).toBe(null);
-            expect(esPalindromo(["a"])).toBe(null);
-            expect(esPalindromo("      ")).toBe(null);
-            expect(esPalindromo("")).toBe(null);
+            expect(isPalindromo(null)).toBe(null);
+            expect(isPalindromo(1)).toBe(null);
+            expect(isPalindromo(undefined)).toBe(null);
+            expect(isPalindromo(2.3)).toBe(null);
+            expect(isPalindromo(true)).toBe(null);
+            expect(isPalindromo({a: ""})).toBe(null);
+            expect(isPalindromo(["a"])).toBe(null);
+            expect(isPalindromo("      ")).toBe(null);
+            expect(isPalindromo("")).toBe(null);
         });
         it("deberia de ser un palindromo", () => {
-            expect(esPalindromo("oxxo")).toBe(true);
-            expect(esPalindromo("anita lava la tina")).toBe(true);
-            expect(esPalindromo("No subas abusón")).toBe(true);
-            expect(esPalindromo("Isaac no rónca así")).toBe(true);
+            expect(isPalindromo("oxxo")).toBe(true);
+            expect(isPalindromo("anita lava la tina")).toBe(true);
+            expect(isPalindromo("No subas abusón")).toBe(true);
+            expect(isPalindromo("Isaac no rónca así")).toBe(true);
         });
         it("deberia de enviar cadenas que no sean palindromos", () => {
-            expect(esPalindromo("No subas, abusón")).toBe(false);
-            expect(esPalindromo("hola hola")).toBe(false);
+            expect(isPalindromo("No subas, abusón")).toBe(false);
+            expect(isPalindromo("hola hola")).toBe(false);
         });
     });
 
@@ -65,12 +65,13 @@ describe("componente - cadenas", () =>{
             expect(cadenaToSequelize(undefined)).toBe(null);
             expect(cadenaToSequelize(2.3)).toBe(null);
             expect(cadenaToSequelize(true)).toBe(null);
-            // expect(cadenaToSequelize(cadena.cad1)).toBe(null);
             expect(cadenaToSequelize({a: ""})).toBe(null);
             expect(cadenaToSequelize(["a"])).toBe(null);
         });
         it("deberia de enviar una cadena con el formato de sequelize 'dialiect://user:pass@example.com:5432/dbname'", () => {
+            const cadena = {cad1: "hola"}    
             expect(cadenaToSequelize("")).toBe(null);
+            expect(cadenaToSequelize(cadena.cad1)).toBe(null);
             expect(cadenaToSequelize("mysql::pass@localhost:3306::")).toBe(null);
             expect(cadenaToSequelize("::")).toBe(null);
             expect(cadenaToSequelize(":::")).toBe(null);
@@ -90,8 +91,8 @@ describe("componente - cadenas", () =>{
             expect(typeof cadenaToSequelize("mssql://root:admin@localhost:5432/spa_oluta")).toBe("object");
         });
         it("deberia de regresar un objeto con los datos definidos", () => {
-            expect(cadenaToSequelize ("mssql://root:admin@localhost:5432/spa_oluta")).toStrictEqual({database: "spa_oluta", dialect: "mssql", host: "localhost", password: "admin", username:  "root"});
-            expect(cadenaToSequelize ("mysql://admin:12345@192.168.100.1:3306/db_any")).toStrictEqual({database:"db_any", username:"admin", password:"12345", host:"192.168.100.1", dialect:"mysql"});
+            expect(cadenaToSequelize ("mssql://root:admin@localhost:5432/spa_oluta")).toStrictEqual({database: "spa_oluta", dialect: "mssql", host: "localhost", password: "admin", username:  "root", port: "5432"});
+            expect(cadenaToSequelize ("mysql://admin:12345@192.168.100.1:3306/db_any")).toStrictEqual({database:"db_any", username:"admin", password:"12345", host:"192.168.100.1", dialect:"mysql", port: "3306"});
         });
     });
 });
